4512a02c1a45b90d73dc5968ab5a6cc125f7396c
super2058 <superMark2058@gmail.com>
2023-12-09 02:52:20 -0500

    Reconfig the backend endpoint

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://plankton-app-vkwlv.ondigitalocean.app/",
+  baseURL: "https://atlasacademics.com/api",
 });
 
 export class HttpService {
